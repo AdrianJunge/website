d0121a329e1eb3a603f87ee31975877ff96d14d2
Adrian Junge <adjun@web.de>
2025-02-23 10:03:08 -0500

    add installer script

---
+++ install_necessary.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+RUBY_VERSION=3.3.7
+CONFIG_FILES=("$HOME/.zshrc" "$HOME/.bashrc")
+
+sudo apt install -y ruby-full
+
+sudo apt install -y rbenv
+sudo apt install -y build-essential libssl-dev libreadline-dev zlib1g-dev libyaml-dev
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+rvm install $RUBY_VERSION
+rbenv global $RUBY_VERSION
+rbenv rehash
+
+sudo apt install -y build-essential procps curl file git
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+for CONFIG in "${CONFIG_FILES[@]}"; do
+    if grep "eval \"\$(rbenv init -)\"" "$CONFIG" > /dev/null; then
+        echo "[-] rbenv init already present in $CONFIG."
+    else
+        echo -e "eval \"\$(rbenv init -)\"" >> "$CONFIG"
+        echo "[*] rbenv init has been added to $CONFIG."
+    fi
+    if grep "eval \"\$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)\"" "$CONFIG" > /dev/null; then
+        echo "[-] Brew already present in $CONFIG."
+    else
+        echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> "$CONFIG"
+        echo "[*] Brew has been added to $CONFIG."
+        eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    fi
+done
+
+brew install watchman
+
+bundle install
+
+gem install overcommit
+exec $SHELL
+overcommit --install
+
+bundle exec rake db:migrate
